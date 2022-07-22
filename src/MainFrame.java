fede184a52aad51f0551612167fe9e62691967a0
yeguarr <61547091+yeguarr@users.noreply.github.com>
2022-07-22 21:03:19 +0300

    Полностью рабочий вертолет.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -41,32 +41,25 @@ public class MainFrame extends JFrame {
         SimulationEnvironment simulationEnvironment = new SimulationEnvironment();
 
         //создаем пропеллеры
-        double lPitch = 2.5;
-        double lRoll = 2.5;
-        AircraftBase.Propeller m1 = new AircraftBase.Propeller(0.1,new Point3D(lPitch,-0.3,lRoll),new Point3D(0,1,0),false,10,"propellerB.obj", simulationEnvironment);
-        AircraftBase.Propeller m2 = new AircraftBase.Propeller(0.1,new Point3D(lPitch,-0.3,-lRoll),new Point3D(0,1,0),true,10,"propellerG.obj",simulationEnvironment);
-        AircraftBase.Propeller m3 = new AircraftBase.Propeller(0.1,new Point3D(-lPitch,-0.3,-lRoll),new Point3D(0,1,0),false,10,"propellerY.obj",simulationEnvironment);
-        AircraftBase.Propeller m4 = new AircraftBase.Propeller(0.1,new Point3D(-lPitch,-0.3,lRoll),new Point3D(0,1,0),true,10,"propellerM.obj",simulationEnvironment);
-        //QuadCopter.Propeller m5 = new QuadCopter.Propeller(1,new Point3D(0,0,2),new Point3D(0,0,-1),true,1000,simulationEnvironment);
+        AircraftBase.Propeller m1 = new AircraftBase.Propeller(0.1,new Point3D(0,-0.5,0),new Point3D(0,1,0),false,10,"propellerM.obj", simulationEnvironment);
+        AircraftBase.Propeller m2 = new AircraftBase.Propeller(0.,new Point3D(-2,0.,0),new Point3D(0,0,-1),true,10,"propellerG.obj",simulationEnvironment);
 
         //создаем сам коптер
         Matrix.m4x4 J = new Matrix.m4x4(0.64,0 ,0, 0, 0, 0.64, 0, 0, 0,0, 1.2,0, 0, 0 ,0 ,1 );
-        AircraftBase copter = new AircraftBase(3,0.1, J, simulationEnvironment);
-        //AircraftBase copter2 = new AircraftBase(3,0.1, J, simulationEnvironment);
-        //добавляем пропеллеры на коптер
-        copter.addPropeller(m1);
-        copter.addPropeller(m2);
-        copter.addPropeller(m3);
-        copter.addPropeller(m4);
-        //copter.addPropeller(m5);
-        //m5.setSpeed(1);
+        AircraftBase helicopter = new AircraftBase(3,0.001, J, simulationEnvironment);
 
-        updater.addTask(copter::calculateForces);
+        //добавляем пропеллеры на коптер
+        helicopter.addPropeller(m1);
+        helicopter.addPropeller(m2);
+
+        //Рассчитываем силы и моменты
+        updater.addTask(helicopter::calculateForces);
+
         //добавляем коптер и пропеллеры в пространство
-        viewer3D.addObject3D(copter.getCopterObject());
-        for (int i = 0; i < copter.getPropellers().size(); i++) {
-            viewer3D.addObject3D(copter.getPropellers().get(i).getObjectStick());
-            viewer3D.addObject3D(copter.getPropellers().get(i).getPropeller3D());
+        viewer3D.addObject3D(helicopter.getCopterObject());
+        for (int i = 0; i < helicopter.getPropellers().size(); i++) {
+            viewer3D.addObject3D(helicopter.getPropellers().get(i).getObjectStick());
+            viewer3D.addObject3D(helicopter.getPropellers().get(i).getPropeller3D());
         }
 
         //создаем и добавляем в пространство точку, в которурую должен прийти коптер
@@ -74,61 +67,42 @@ public class MainFrame extends JFrame {
         viewer3D.addObject3D(OBJRef);
 
         //добовляем следящую за коптером линию
-        LineTracer lineTracer = new LineTracer(500,4,copter.getPosition());
-        updater.addTask(()->lineTracer.updatePosition(copter.getPosition()));
+        LineTracer lineTracer = new LineTracer(500,4,helicopter.getPosition());
+        updater.addTask(()->lineTracer.updatePosition(helicopter.getPosition()));
         viewer3D.addObject3D(lineTracer.getLinesObject());
 
         ///////////////////////// CONTROLLER
         PID pid1 = new PID(0.8,0.4,2, simulationEnvironment);
-        PID pid2 = new PID(3,0,0, simulationEnvironment);
-        PID pid3 = new PID(3,0,0, simulationEnvironment);
-        PID pid4 = new PID(0.3,0.3,8, simulationEnvironment);
+        PID pid2 = new PID(1,0.01,0, simulationEnvironment);
+
 
         AtomicReference<Double> pitch = new AtomicReference<>(0.);
 
-        copter.setAngle(Utils.eulerAnglesToQuaternion(new Point3D(0,pitch.get() ,0)));
+        helicopter.setAngle(Utils.eulerAnglesToQuaternion(new Point3D(0,pitch.get() ,0)));
         Object3D lineArrowObject = new Object3D();
         viewer3D.addObject3D(lineArrowObject);
 
         updater.addTask( () -> {
-            Point3D refPosition = new Point3D(20,1000,20);
-            OBJRef.setPosition(Utils.rotY(pitch.get()).multiply(refPosition.scale(1,0,1)));
-            Point3D angle = Utils.quaternionToEulerAngles(copter.getAngle());
-            double copterY = angle.getY();
-            Quaternion rotatedCopter = (copter.getAngle());
-            Point3D distance = refPosition.add(Utils.rotY(-copterY).multiply(copter.getPosition().multiply(-1)));
-            Quaternion errorRotation =  Utils.getQuaternionToPoint(distance ).multiply(rotatedCopter.conjugate());
-            System.out.println(distance);
+            Point3D position = new Point3D(10,10,10);
+            OBJRef.setPosition(position.scale(1,-1,1));
+            double neededAngle = 45;
 
-            //System.out.println(Arrays.deepToString(Utils.quaternionRotation(copter.getAngle()).getMatrixArray()));
-            //Quaternion rotated = (Utils.eulerAnglesToQuaternion(new Point3D(0,copterY,0)).conjugate().multiply(Utils.eulerAnglesToQuaternion(angle)));
+            double errX = position.getX()-helicopter.getPosition().getX();
+            double errY = -(position.getY()-helicopter.getPosition().getY());
+            double errZ = position.getZ()-helicopter.getPosition().getZ();
 
-            //System.out.println(Utils.quaternionToEulerAngles(copter.getAngle()) + "    " + Utils.quaternionToEulerAngles(rotated));
+            Point3D errVec = Utils.rotY(-(neededAngle)).multiply(new Point3D(errX,errY,errZ));
 
-            lineArrowObject.setFaces(new Line3D[]{new Line3D(copter.getPosition(),Utils.getNormalVectorToPoint(refPosition))});
-//Quaternion angleRotated = Utils.eulerAnglesToQuaternion(new Point3D(0,0,0)).multiply(copter.getAngle()).multiply(Utils.eulerAnglesToQuaternion(new Point3D(0,0,0)));
-            //Quaternion angleRotated = Utils.eulerAnglesToQuaternion(new Point3D(0,copterY,0)).multiply(rotated);
-            //Quaternion angleRotated =  Utils.eulerAnglesToQuaternion(new Point3D(0,0,angle.getZ())).multiply(Utils.eulerAnglesToQuaternion(new Point3D(0,angle.getY(),angle.getZ())).conjugate().multiply(Utils.eulerAnglesToQuaternion(angle)));
-            //Quaternion errorRotationOther = Utils.getQuaternionToPoint( refPosition.add(Utils.rotY(copterY).multiply(copter.getPosition())) ).multiply(angleRotated.conjugate());
+            double angleX = Math.cos(Math.atan2(errVec.getY(),errVec.getX()));
+            double angleZ = Math.cos(Math.atan2(errVec.getY(),errVec.getZ()));
 
+            double errHover = pid1.calculateControl(-helicopter.getPosition().getY(),position.getY());
+            double errRot = pid2.calculateControl(Math.acos(helicopter.getAngle().getW())*2, Math.toRadians(neededAngle));
+            m1.setSpeed(errHover);
+            m2.setSpeed(errRot);
 
-            Point3D vector = new Point3D(errorRotation.getX(),0,errorRotation.getZ());
-
-            double err1 = pid1.calculateControl(-copter.getPosition().getY(),1000-refPosition.getY());
-            double err2 = 0;//pid2.calculateControl(vector.getX(),0);
-            double err3 = 0;//pid3.calculateControl(vector.getZ(),0);
-            double err4 = 0;//pid4.calculateControl(vector.getY(),0);
-
-            m1.setSpeed(err1-err4-err2+err3);
-            m2.setSpeed(err1+err4+err2+err3);
-            m3.setSpeed(err1-err4+err2-err3);
-            m4.setSpeed(err1+err4-err2-err3);
-
-            /*m1.setSpeed(err1);
-            m2.setSpeed(err1);
-            m3.setSpeed(err1);
-            m4.setSpeed(err1);*/
-
+            Quaternion q = new Quaternion(Utils.eulerAnglesToQuaternion(new Point3D(-angleZ,0,angleX))).multiply(helicopter.getAngle().conjugate());
+            m1.setForce(Utils.quaternionRotation(q).multiply(new Point3D(0,1,0)));
         });
         ///////////////////////// CONTROLLER END
 
@@ -138,13 +112,13 @@ public class MainFrame extends JFrame {
 
         //обнавляем пропеллеры
         updater.addTask(() -> {
-            for (AircraftBase.Propeller propeller : copter.getPropellers())
+            for (AircraftBase.Propeller propeller : helicopter.getPropellers())
                 propeller.updateThrust();
         });
 
         //обналяем и интегрируем силы коптера
 
-        updater.addTask(copter::updateIntegration);
+        updater.addTask(helicopter::updateIntegration);
 
         //управление с клавиатуры
 
@@ -153,64 +127,19 @@ public class MainFrame extends JFrame {
             m1.setForce(m1.getForce().add(new Point3D(0, 0,pitch.get())));
         });
 
-        controlsGUI.bindAKey(KeyEvent.VK_SHIFT, () -> {
-            m1.setSpeed(m1.getThrust()-1.1f);
-            m2.setSpeed(m2.getThrust()+1.1f);
-            m3.setSpeed(m3.getThrust()-1.1f);
-            m4.setSpeed(m4.getThrust()+1.1f);
-        });
-        controlsGUI.bindAKey(KeyEvent.VK_CONTROL, () -> {
-            m1.setSpeed(m1.getThrust()+1.1f);
-            m2.setSpeed(m2.getThrust()-1.1f);
-            m3.setSpeed(m3.getThrust()+1.1f);
-            m4.setSpeed(m4.getThrust()-1.1f);
-        });
-        controlsGUI.bindAKey(KeyEvent.VK_LEFT, () -> {
-            m1.setSpeed(m1.getThrust()+1.1f);
-            m2.setSpeed(m2.getThrust()-1.1f);
-            m3.setSpeed(m3.getThrust()-1.1f);
-            m4.setSpeed(m4.getThrust()+1.1f);
-        });
-        controlsGUI.bindAKey(KeyEvent.VK_UP, () -> {
-            m1.setSpeed(m1.getThrust()-1.1f);
-            m2.setSpeed(m2.getThrust()-1.1f);
-            m3.setSpeed(m3.getThrust()+1.1f);
-            m4.setSpeed(m4.getThrust()+1.1f);
-        });
-        controlsGUI.bindAKey(KeyEvent.VK_RIGHT, () -> {
-            m1.setSpeed(m1.getThrust()-1.1f);
-            m2.setSpeed(m2.getThrust()+1.1f);
-            m3.setSpeed(m3.getThrust()+1.1f);
-            m4.setSpeed(m4.getThrust()-1.1f);
-        });
-        controlsGUI.bindAKey(KeyEvent.VK_DOWN, () -> {
-            m1.setSpeed(m1.getThrust()+1.1f);
-            m2.setSpeed(m2.getThrust()+1.1f);
-            m3.setSpeed(m3.getThrust()-1.1f);
-            m4.setSpeed(m4.getThrust()-1.1f);
-        });
-        controlsGUI.bindAKey(KeyEvent.VK_N, () -> {
-            m1.setSpeed(0);
-            m2.setSpeed(0);
-            m3.setSpeed(0);
-            m4.setSpeed(0);
-        });
-
         // поворот камеры при зажатой кнопке M
         controlsGUI.bindAKey(KeyEvent.VK_M,
-                () -> camera.setPosition(copter.getPosition().multiply(-1).add(
+                () -> camera.setPosition(helicopter.getPosition().multiply(-1).add(
                 new Point3D((20*Math.sin(Math.toRadians(camera.getRotation().getY()))*Math.cos(Math.toRadians(camera.getRotation().getX()))),
                          (-20*Math.sin(Math.toRadians(camera.getRotation().getX()))),
                          (-20*Math.cos(Math.toRadians(camera.getRotation().getY()))*Math.cos(Math.toRadians(camera.getRotation().getX())))))));
         // Cam rotation end
 
         //обнавляем позицию объектов коптера и пропеллеров
-        updater.addTask(copter::updateObjectAndPropellers);
+        updater.addTask(helicopter::updateObjectAndPropellers);
 
         //выводим FPS
-        updater.addTask(() -> {
-            textArea.setText(String.valueOf(updater.getFrames()));
-        });
+        updater.addTask(() -> textArea.setText(String.valueOf(updater.getFrames())));
 
         ///////// END OF TEST ZONE
 
